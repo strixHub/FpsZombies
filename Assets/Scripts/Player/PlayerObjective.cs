3b7a87ab9ef7d3c8a4c29aef2f2f499f15662293
Mou <markusmou51@gmail.com>
2023-05-03 17:35:43 +0900

    improved UI, to change UI for the target word

---
+++ Assets/Scripts/Player/PlayerObjective.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerObjective : MonoBehaviour
+{
+    public TMP_Text objective;
+    void Start()
+    {
+        
+        var random = new System.Random();
+        int rndIndex = random.Next(GameMng.SortedList.Count);
+        objective.text = GameMng.SortedList[rndIndex].wordToLearn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
